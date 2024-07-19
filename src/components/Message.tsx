cf66f2adad3bc8a59a8278033ee0e0ce12e30e22
BekmurzinTimur <49587448+BekmurzinTimur@users.noreply.github.com>
2024-07-19 15:13:02 +0500

    set max width of message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -9,7 +9,7 @@ const Message = ({ text, timestamp, user }: MessageType) => {
     user === 'Me' ? 'self-end bg-blue-500 text-white' : 'self-start bg-gray-200'
   const timestampClass = user === 'Me' ? 'text-gray-200' : 'text-gray-800'
   return (
-    <div className={`p-2 my-2 rounded-md ${alignClass}`}>
+    <div className={`p-2 my-2 rounded-md ${alignClass} max-w-xs`}>
       <div>{text}</div>
       <div className={'text-xs ' + timestampClass}>
         {new Date(timestamp).toLocaleTimeString()}
